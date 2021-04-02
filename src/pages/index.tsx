83ed89147983c8370fe289b8e448780811163092
Ricardo <ricardoinliner@yahoo.com>
2021-04-02 11:42:33 -0300

    load stripe price with GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styles from './home.module.scss';
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe';
-
 
 interface HomeProps {
   product: {
@@ -13,7 +12,6 @@ interface HomeProps {
 }
 
 export default function Home({product}: HomeProps) {
-
 
   return (
     <>
@@ -34,14 +32,12 @@ export default function Home({product}: HomeProps) {
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
-    
   )
 }
 
-export const getServerSideProps : GetServerSideProps = async () => {
+export const getStaticProps : GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1Ibmv4IzWQfCYXmGa6X3pGf6')
-
   const product = {
     priceId: price.id,
     amount:  new Intl.NumberFormat('en-US', {
@@ -53,6 +49,7 @@ export const getServerSideProps : GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
