921720c30e6f48e83110340bd522e298c5fdfb57
Ricardo <ricardoinliner@yahoo.com>
2021-04-06 10:29:02 -0300

    configure FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,11 +7,7 @@ import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-
     const [session] = useSession();
-
-    console.log(session);
-
 
     return session ? (
         <button 
