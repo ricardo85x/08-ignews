0adf2c936e0dd3c5aebdea50ac62371c618c433d
Ricardo <ricardoinliner@yahoo.com>
2021-04-08 12:03:33 -0300

    update subscription status on faunadb from stripe

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,7 +5,8 @@ import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
     subscriptionId: string, 
-    customerId: string
+    customerId: string,
+    createAction: boolean,
     
 ) {
   //user_by_stripe_customer_id
@@ -33,10 +34,29 @@ export async function saveSubscription(
 
   }
 
-  await fauna.query(
-      q.Create(
-          q.Collection('subscriptions'),
-          { data: subscriptionData }
+  
+  if (createAction) {
+    await fauna.query(
+        q.Create(
+            q.Collection('subscriptions'),
+            { data: subscriptionData }
+        )
+    )
+  }  else {
+      await fauna.query(
+          q.Replace(
+              q.Select(
+                "ref",
+                q.Get(
+                    q.Match(
+                        q.Index('subscription_by_id'),
+                        subscriptionId
+                    )
+                )
+              ),
+              { data: subscriptionData }
+          )
       )
-  )
+  }
+
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,7 +24,11 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.created',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
+    
 ])
 
 export default  async (req: NextApiRequest, res: NextApiResponse) => {
@@ -51,13 +55,29 @@ export default  async (req: NextApiRequest, res: NextApiResponse) => {
             try { 
 
                 switch(type) {
+
+                    case 'customer.subscription.created':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        console.log(type)
+                        const subscription = event.data.object as Stripe.Subscription
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            type === 'customer.subscription.created'
+                        )
+
+                        break
+
                     case 'checkout.session.completed':
 
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                         )
 
 
