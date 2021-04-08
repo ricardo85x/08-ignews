04b61fb3bf6ad13febecfd8a60f9b068fa31364e
Ricardo <ricardoinliner@yahoo.com>
2021-04-08 11:21:03 -0300

    save subscription on faunadb

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
 import Stripe from 'stripe'
 import { stripe } from '../../services/stripe'
+import { saveSubscription } from './_lib/manageSubscription'
 
 async function buffer(readable: Readable) {
     const chunks = []
@@ -14,7 +15,6 @@ async function buffer(readable: Readable) {
 
     return Buffer.concat(chunks)
 }
-
 
 // set bodyParser config to false, when Next api should use stream
 export const config = {
@@ -47,7 +47,29 @@ export default  async (req: NextApiRequest, res: NextApiResponse) => {
 
 
         if (relevantEvents.has(type)){
-            console.log("Evento recebido", event)
+
+            try { 
+
+                switch(type) {
+                    case 'checkout.session.completed':
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString()
+                        )
+
+
+                        break
+                    default:
+                        throw new Error('Unhandled event')
+                }
+
+            } catch (err) {
+                return res.json( { message : 'Webhook handled failed'})
+            }
+            
         }
 
 
