41d79f3a712b49546089e45ca3d057fe0477b1b5
Ricardo <ricardoinliner@yahoo.com>
2021-04-12 11:56:52 -0300

    using the cloneElement from react

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,24 @@
 import { SignInButton } from '../SignInButton';
-import Link from 'next/link'
 import styles from './styles.module.scss';
+import React from 'react';
+import { ActiveLink } from '../ActiveLink';
+
+
+
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active} >Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
-                        <a >Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active}  href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 
                 <SignInButton />
